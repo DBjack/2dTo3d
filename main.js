0c526594bcd9799dd15aa7e901326c077e1e4bd6
wzj <1078122958@qq.com>
2023-09-05 00:39:12 +0800

    增加着色器

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 
+// 创建一个场景
 const scene = new THREE.Scene();
 
+// 创建一个相机
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -18,6 +20,7 @@ const axesHelper = new THREE.AxesHelper(5);
 // 将坐标轴添加到场景中
 scene.add(axesHelper);
 
+//将相机添加到场景中
 scene.add(camera);
 
 // 创建一个材质 颜色为红色
@@ -28,14 +31,20 @@ const material = new THREE.MeshBasicMaterial({
 // 加载纹理
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load("./models/picTo3d/sunset.jpg");
+const depthTexture = textureLoader.load("./models/picTo3d/sunset-depth.jpg");
+
+// 创建一个鼠标二维向量
+const mouse = new THREE.Vector2();
 
 // 创建一个着色器材质
 const shaderMaterial = new THREE.ShaderMaterial({
+  // 顶点着色器
   vertexShader: `
     void main() {
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `,
+  // 片元着色器
   fragmentShader: `  
     void main() {
       gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
@@ -43,7 +52,16 @@ const shaderMaterial = new THREE.ShaderMaterial({
   `,
   uniforms: {
     uTexture: {
+      // 纹理
       value: texture,
+    },
+    uDepthTexture: {
+      // 深度纹理
+      value: depthTexture,
+    },
+    uMouse: {
+      // 鼠标二维向量
+      value: mouse,
     },
   },
 });
@@ -68,11 +86,14 @@ document.body.appendChild(renderer.domElement);
 
 // 渲染
 function animate() {
+  // 递归调用渲染函数
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
+  // 渲染场景
   renderer.render(scene, camera);
 }
 
+// 调用渲染函数
 animate();
